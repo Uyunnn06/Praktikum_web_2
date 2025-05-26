08ee4f915470e98867cbb8901d0a554c21f5e493
Uyunnn06 <Yuyunauliaafriyanti123@gmail.com>
2025-05-26 14:51:45 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (env('APP_ENV') == 'production'){
-            URL::forceSchema('https');
+            URL::forceScheme('https');
         }
         
         Gate::define("view-student", function (User $user) {
